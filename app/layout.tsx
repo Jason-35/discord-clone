427f8de28c5e42a23975c6fe8d678fe2ce69ec7c
Jason <jsonwu34@gmail.com>
2023-11-08 16:29:15 -0500

    added light and dark theme with toggles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from '@/components/provider/theme-provider'
+import { cn } from '@/lib/utils'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -16,10 +18,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
         <ClerkProvider>
-          <body className={font.className}>
-            {children}
+          <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
+            <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false} storageKey='discord-theme'>
+              {children}
+            </ThemeProvider>
           </body>
         </ClerkProvider>
       </html>
