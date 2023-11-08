4c5a3bd98e87ae6c498c342c1021b07d69a2b490
Jason <jsonwu34@gmail.com>
2023-11-08 15:16:32 -0500

    clerk authentication works added a UserButton in main folder so user can logout centered the authentication card

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 const AuthLayout = ({children} : {children : React.ReactNode}) => {
-    return ( <div className="bg-red-500 h-full"> Auth layout here {children} </div> );
+    return ( <div className="h-full flex justify-center items-center">{children}</div> );
 }
  
 export default AuthLayout;
