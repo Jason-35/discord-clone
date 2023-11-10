91a56ccfb9ee2b93cf2686374a72a4a58cdad335
Jason <jsonwu34@gmail.com>
2023-11-10 14:53:41 -0500

    can create and redirect to a server created an api for making post request for server

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -12,9 +12,13 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+
+import axios from "axios";
 
 const InitialModal = () => {
     const [isMounted, setIsMounted] = useState(false)
+    const router = useRouter()
 
     useEffect(()=>{
         setIsMounted(true)
@@ -40,7 +44,14 @@ const InitialModal = () => {
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = async(values: z.infer<typeof formSchema>) => {
-        console.log(values)
+        try {
+            await axios.post("/api/servers", values)
+            form.reset()
+            router.refresh();
+            window.location.reload();
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     if(!isMounted){
