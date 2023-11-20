0cc8176ee885481991aa0b52ee75182f7fd5ef66
Jason <jsonwu34@gmail.com>
2023-11-20 17:02:53 -0500

    setup socket using pages created input for messages form for submitting messages

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/provider/theme-provider'
 import { cn } from '@/lib/utils'
 import { ModalProvider } from '@/components/provider/modal-provider'
+import { SocketProvider } from '@/components/provider/socket-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -23,8 +24,10 @@ export default function RootLayout({
         <ClerkProvider>
           <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
             <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false} storageKey='discord-theme'>
-              <ModalProvider />
-              {children}
+              <SocketProvider>
+                <ModalProvider />
+                {children}
+              </SocketProvider>
             </ThemeProvider>
           </body>
         </ClerkProvider>
